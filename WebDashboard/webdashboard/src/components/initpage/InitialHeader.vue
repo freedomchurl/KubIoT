28a5fe8f922447283bdfb912c8f4516fbb17bb90
freedomchurl <freedomchurl@naver.com>
2020-07-30 02:01:22 +0900

    Vue CLI Update

---
+++ WebDashboard/webdashboard/src/components/initpage/InitialHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+        <h1>KubIoT 초기 설정 페이지</h1>
+    </header>
+</template>
+
+<script>
+export default{
+
+}
+</script>
+
+<style scoped>
+    h1{
+        color:#a205b1;
+        font-weight: 900;
+        margin:2.5rem 0 1.5rem;
+    }
+</style>

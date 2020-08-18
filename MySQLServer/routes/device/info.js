05ada13b281616987b9f4b120cc4dd9776ca887b
freedomchurl <freedomchurl@naver.com>
2020-08-19 08:41:43 +0900

    Change

--- MySQLServer/routes/device/info.js
+++ MySQLServer/routes/device/info.js
@@ -15,44 +15,6 @@ charset : 'utf8'
 
 
 router.use(express.json());
-router.get('/',function(req,res){
-
-		var key = req.query.pageinfo;
-		pool.getConnection(function(err,conn){
-				if(err){
-				if(conn){
-				conn.release();
-				}
-				throw err;
-				}
-	//			key가 2라면, 2번째 페이지. 페이지당 20개니까,
-				//(key-1)*20 ~ 20
-				var startindex = (key-1)*20;
-					var exec = conn.query('select * from device order by time desc limit ?,20',startindex,function(err,result){
-						conn.release();
-						
-						res.header("Access-Control-Allow-Headers","Authorization");
-						res.header("Access-Control-Expose-Headers","*");
-						if(err){
-						res.send({status:false,payload:null});
-						}
-						else{	
-						if(result.length){
-						res.send({status:true,payload:result});
-						}
-						else
-						{
-						res.send({status:true,paylod:null});
-						}
-						}
-
-						});
-
-		});
-
-});
-
-// /device/info/devicenum get
 router.get('/devicenum',function(req,res){
 	//var id = req.body.adminid;
 	//var pwd = req.body.adminpwd;
@@ -91,6 +53,46 @@ router.get('/devicenum',function(req,res){
 
 	});
 });
+
+router.get('/',function(req,res){
+
+		var key = req.query.pageinfo;
+		pool.getConnection(function(err,conn){
+				if(err){
+				if(conn){
+				conn.release();
+				}
+				throw err;
+				}
+	//			key가 2라면, 2번째 페이지. 페이지당 20개니까,
+				//(key-1)*20 ~ 20
+				var startindex = (key-1)*20;
+					var exec = conn.query('select * from device order by time desc limit ?,20',startindex,function(err,result){
+						conn.release();
+						
+						res.header("Access-Control-Allow-Headers","Authorization");
+						res.header("Access-Control-Expose-Headers","*");
+						if(err){
+						res.send({status:false,payload:null});
+						}
+						else{	
+						if(result.length){
+						res.send({status:true,payload:result});
+						}
+						else
+						{
+						res.send({status:true,paylod:null});
+						}
+						}
+
+						});
+
+		});
+
+});
+
+// /device/info/devicenum get
+
 
 
 module.exports = router;
